395fd0c036d350fa669856eef1183b6063583999
John Harney <jfharney@gmail.com>
2026-02-02 19:01:53 -0500

    WIP: changes to the multiple server scripts

--- script/my_multiple_echo_servers.bat
+++ script/my_multiple_echo_servers.bat
@@ -1,4 +1,4 @@
 @echo off
 call "%~dp0config.bat"
 echo Starting Echo Services on ports 6001 and 6002 in separate tabs...
-wt new-tab powershell -Command "python %ECHO_SERVER_PATH% 6001" ; new-tab powershell -Command "python %ECHO_SERVER_PATH% 6002"
+wt new-tab powershell -Command "python %ECHO_SERVER_PATH% 6001" ; new-tab powershell -Command "python %ECHO_SERVER2_PATH% 6002"
--- script/stop_echo_servers.bat
+++ script/stop_echo_servers.bat
@@ -1,4 +1,7 @@
 @echo off
 echo Stopping all echo server processes...
+rem powershell -Command "Get-CimInstance Win32_Process | Where-Object {$_.CommandLine -like '*echo_server.py*' -or $_.CommandLine -like '*echo_server2.py*'} | ForEach-Object {Stop-Process -Id $_.ProcessId -Force}"
 powershell -Command "Get-CimInstance Win32_Process | Where-Object {$_.CommandLine -like '*echo_server.py*'} | ForEach-Object {Stop-Process -Id $_.ProcessId -Force}"
+powershell -Command "Get-CimInstance Win32_Process | Where-Object {$_.CommandLine -like '*echo_server2.py*'} | ForEach-Object {Stop-Process -Id $_.ProcessId -Force}"
+
 echo Echo servers stopped.
